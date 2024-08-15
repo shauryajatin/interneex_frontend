f557036cabb5e0bfa0f08243919f9f96c748a0d5
shankarjatin <shankarjatin1005@gmail.com>
2024-08-16 00:16:31 +0530

    constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 // utils/apiUtil.js
 
-// const API_BASE_URL =  'https://backend.interneex.com/api';
+const API_BASE_URL =  'https://backend.interneex.com/api';
 
-const API_BASE_URL =  'http://localhost:3001/api';
+// const API_BASE_URL =  'http://localhost:3001/api';
 
 
 export default API_BASE_URL;
